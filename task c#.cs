855e1411eff354a92fc4bf1c78f7514058b3d282
Aghayev0 <tu6k6wzh2@code.edu.az>
2022-10-31 22:46:50 +0400

    TAsk

---
+++ task c#.cs
@@ -0,0 +1,44 @@
+﻿//int sum = 0;
+//int number = 43;
+//for (int i = 2; i <= number ; i *= 2)
+//{
+
+//    if (i % 2 == 0)
+//        sum++;
+//}
+//Console.WriteLine(sum);
+//if ((Math.Pow(2, sum+1)) - number < number - (Math.Pow(2, sum)))
+//{
+//	Console.WriteLine(sum + 1);
+//}
+//if ((Math.Pow(2, sum + 1)) - number >= number - (Math.Pow(2, sum)))
+//{
+//    Console.WriteLine(sum);
+//}
+
+//int sum = 0;
+//int number = 77;
+//int a=0;    
+//int b=0;
+//for (int i = 0; i < 46; i++)
+//{
+//    a = number / 10;
+//    b = number % 10;
+//} sum = a + b;
+//Console.WriteLine(sum);
+ 
+
+//int sum = 0;
+//int num = 8;
+//{
+//    if ((num % 2 == 0 || (num % 3 == 0) || (num % 5 == 0) ))
+//    Console.WriteLine("murekkeb ededdir");
+//    else
+//    {
+//        Console.WriteLine("sade ededdir");
+//    }    
+//}
+
+
+
+
